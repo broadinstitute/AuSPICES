4d261e376e8936000bee906a7faaf841843811c8
ErinWeisbart <eweisbar@broadinstitute.org>
2021-09-15 09:51:30 -0700

    work on pe2loaddata

--- 1_pe2loaddata/lambda_function.py
+++ 1_pe2loaddata/lambda_function.py
@@ -1,60 +1,69 @@
 import boto3
+import botocore
 import os
 import sys
-import json
-from pe2loaddata.__main__ import main as pe2loaddata
+import subprocess
+import xml.etree.ElementTree as ET
+from pe2loaddata.__main__ import headless as pe2loaddata
 
 s3 = boto3.client("s3")
 
-def lambda_handler(event, lambda_context):
-    fullplate = event['trigger']['plate']
-    plate = fullplate.split("__")[0]
-    project_name = event['project_name']
-    batch = event['batch']
-    bucket = event['bucket']
 
-    # Update config.yml based on Index.ids.xml file
-    s3xmlfilepath = f"projects/{project_name}/{batch}/images/{fullplate}/Images/Index.idx.xml"
+def lambda_handler(event, lambda_context):
+    fullplate = event["trigger"]["plate"]
+    plate = fullplate.split("__")[0]
+    project_name = event["project_name"]
+    batch = event["batch"]
+    bucket = event["bucket"]
+
+    # (Eventually) Update config.yml based on Index.ids.xml file
+    s3xmlfilepath = (
+        f"projects/{project_name}/{batch}/images/{fullplate}/Images/Index.idx.xml"
+    )
     localxmlfilepath = "/tmp/Index.idx.xml"
-    #s3.download_file(bucket, s3xmlfilepath, localxmlfilepath)
-    with open(localxmlfilepath, 'wb') as f:
+
+    with open(localxmlfilepath, "wb") as f:
         try:
             s3.download_fileobj(bucket, s3xmlfilepath, f)
         except botocore.exceptions.ClientError as error:
             print("Can't find the xml file. Something's up.")
             return
-    with open(localxmlfilepath, encoding='utf-8-sig') as input_xml:
-        xmlfile = json.load(input_xml)
-    xmlfile
-    """
-    localyamlpath = "/tmp/config.yml"
-    # open yaml
-    # edit yaml to match xml
+    with open(localxmlfilepath, "r") as input_xml:
+        xmlfile = ET.parse(input_xml)
+        root = xmlfile.getroot()
+
+    yamlpath = "./config.yml"
+    # (Eventually) Open yaml
+    # (Eventually) Edit yaml to match xml
+
+    # Setup for pe2loaddata
+    if not os.path.exists(f"/mnt/efs/{project_name}/workspace/"):
+        os.mkdir(f"/mnt/efs/{project_name}/workspace/")
+    if not os.path.exists(f"/mnt/efs/{project_name}/workspace/log/{batch}"):
+        os.mkdir(f"/mnt/efs/{project_name}/workspace/log/{batch}")
+    if not os.path.exists(f"/mnt/efs/{project_name}/workspace/images"):
+        os.mkdir(f"/mnt/efs/{project_name}/workspace/images")
+    if not os.path.islink(f"/mnt/efs/{project_name}/workspace/images/{batch}"):
+        os.symlink(f"/bucket/projects/{project_name}/{batch}/images/", f"/mnt/efs/{project_name}/workspace/images/{batch}")
 
     # Trigger pe2loaddata
-    pe2loaddata(config.yml f"~/efs/{project_name}/workspace/load_data_csv/{batch}/{plate}/load_data.csv" \
-    --index-directory f"~/efs/{project_name}/workspace/images/{batch}/{fullplate}/Images" \
-    --illum \
-    --illum-directory f"/home/ubuntu/bucket/projects/{project_name}/{batch}/illum/{plate}" \
-    --plate-id f"{plate}" \
-    --illum-output f"~/efs/{project_name}/workspace/load_data_csv/{batch}/{plate}/load_data_with_illum.csv" \
-    --sub-string-out f"efs/{project_name}/workspace/images/{batch}" \
-    --sub-string-in f"bucket/projects/{project_name}/{batch}/images")
-    """
+    output = f"/mnt/efs/{project_name}/workspace/load_data_csv/{batch}/{plate}/load_data.csv"
+    index_directory = f"/mnt/efs/{project_name}/workspace/images/{batch}/{fullplate}/Images"
+    illum_directory = (
+        f"/home/ubuntu/bucket/projects/{project_name}/{batch}/illum/{plate}"
+    )
+    illum_output = f"/mnt/efs/{project_name}/workspace/load_data_csv/{batch}/{plate}/load_data_with_illum.csv"
+    sub_string_out = f"mnt/efs/{project_name}/workspace/images/{batch}"
+    sub_string_in = f"bucket/projects/{project_name}/{batch}/images"
 
-
-
-
-"""
-mkdir -p ~/efs/${PROJECT_NAME}/workspace/scratch/${BATCH_ID}/
-PLATES=$(readlink -f ~/efs/${PROJECT_NAME}/workspace/scratch/${BATCH_ID}/plates_to_process.txt)
-FULL_PLATES=$(readlink -f ~/efs/${PROJECT_NAME}/workspace/scratch/${BATCH_ID}/full_plates_to_process.txt)
-ls ~/efs/${PROJECT_NAME}/workspace/images/${BATCH_ID}/ | cut -d '_' -f 1 >> $PLATES
-ls ~/efs/${PROJECT_NAME}/workspace/images/${BATCH_ID}/ >> $FULL_PLATES
-Check that your plate names are correct by nano $PLATES and nano $FULL_PLATES. If your plate names contain underscores, you may need to fix the simplified plate names. Both should have the same number of rows as the number of plates in your batch.
-
-SAMPLE_PLATE_ID=PLATE_NAME_1
-This can be any single plate name, again using the portion of the name that is before the double underscore __.
-SAMPLE_FULL_PLATE_NAME=FULL_PLATE_NAME_1
-This can be any single plate name, this time using the full name.
-"""
+    pe2loaddata(
+    yamlpath,
+    output,
+    index_directory=index_directory,
+    illum=True,
+    illum_directory=illum_directory,
+    plate_id=plate,
+    illum_output=illum_output,
+    sub_string_out=sub_string_out,
+    sub_string_in=sub_string_in
+    )
