19770eff598065578d6a79866819d323eaf94ceb
ErinWeisbart <eweisbar@broadinstitute.org>
2021-09-16 13:58:32 -0700

    add channeldict

--- 1_pe2loaddata/lambda_function.py
+++ 1_pe2loaddata/lambda_function.py
@@ -1,9 +1,6 @@
 import boto3
 import botocore
-#import os
-#import sys
-#import subprocess
-import xml.etree.ElementTree as ET
+import shutil
 from pe2loaddata.__main__ import headless as pe2loaddata
 import yaml
 
@@ -16,29 +13,46 @@ def lambda_handler(event, lambda_context):
     project_name = event["project_name"]
     batch = event["batch"]
     bucket = event["bucket"]
-
-    """
     channeldict = event["channeldict"]
 
-
-    if channeldict == 'Standard_1BF':
-    if channeldict == 'Standard_3BF':
+    # Create channel map
+    if channeldict == "Standard_1BF":
+        channelmap = {
+            "HOECHST 33342": "OrigDNA",
+            "Alexa 568": "OrigAGP",
+            "Alexa 647": "OrigMito",
+            "Alexa 488": "OrigER",
+            "488 long": "OrigRNA",
+            "Brightfield": "OrigBrightfield",
+        }
+    elif channeldict == "Standard_3BF":
+        channelmap = {
+            "HOECHST 33342": "OrigDNA",
+            "Alexa 568": "OrigAGP",
+            "Alexa 647": "OrigMito",
+            "Alexa 488": "OrigER",
+            "488 long": "OrigRNA",
+            "Brightfield": "OrigBrightfield",
+            "Brightfield H": "OrigBrightfield_H",
+            "Brightfield L": "OrigBrightfield_L",
+        }
     else:
         if type(channeldict) is not dict:
-            print("In metadata: enter string for existing map into channeldict enter new map as dictionary.")
+            print(
+                "In metadata: enter string for existing map into channeldict enter new map as dictionary."
+            )
             return
-    else:
+        else:
+            channelmap = channeldict
 
-
-    # Load and edit config file
-    config_file = "./config.yml"
+    # Load and edit config file with channel map
+    shutil.copy("./config.yml", "/tmp/config.yml")
+    config_file = "/tmp/config.yml"
     with open(config_file, "r") as fd:
         config = yaml.load(fd, Loader=yaml.BaseLoader)
-    config['channels'] =
-    # Write to yaml
-    """
-
-    config_file = "./config.yml"
+    config["channels"] = channelmap
+    with open(config_file, "w") as fd:
+        yaml.dump(config, fd, Dumper=yaml.Dumper)
 
     # Trigger pe2loaddata
     output = f"/tmp/load_data.csv"
@@ -50,21 +64,23 @@ def lambda_handler(event, lambda_context):
     sub_string_in = "/home/ubuntu/bucket/projects"
 
     pe2loaddata(
-    config_file,
-    output,
-    index_directory=index_directory,
-    index_file=index_file,
-    illum=True,
-    illum_directory=illum_directory,
-    plate_id=plate,
-    illum_output=illum_output,
-    sub_string_out=sub_string_out,
-    sub_string_in=sub_string_in,
-    search_subdirectories=True
+        config_file,
+        output,
+        index_directory=index_directory,
+        index_file=index_file,
+        illum=True,
+        illum_directory=illum_directory,
+        plate_id=plate,
+        illum_output=illum_output,
+        sub_string_out=sub_string_out,
+        sub_string_in=sub_string_in,
+        search_subdirectories=True,
     )
 
     # Upload .csvs to S3
-    output_on_bucket_name = f"projects/{project_name}/workspace/load_data_csv/{batch}/{plate}/load_data.csv"
+    output_on_bucket_name = (
+        f"projects/{project_name}/workspace/load_data_csv/{batch}/{plate}/load_data.csv"
+    )
     illum_output_on_bucket_name = f"projects/{project_name}/workspace/load_data_csv/{batch}/{plate}/load_data_with_illum.csv"
     with open(output, "rb") as a:
         s3.put_object(Body=a, Bucket=bucket, Key=output_on_bucket_name)
