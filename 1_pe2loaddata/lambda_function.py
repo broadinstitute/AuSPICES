3eb4aa4cb087eb89d4ec3f1700a707bff4e219a0
ErinWeisbart <eweisbar@broadinstitute.org>
2021-09-16 13:16:19 -0700

    it's alive

--- 1_pe2loaddata/lambda_function.py
+++ 1_pe2loaddata/lambda_function.py
@@ -1,10 +1,11 @@
 import boto3
 import botocore
-import os
-import sys
-import subprocess
+#import os
+#import sys
+#import subprocess
 import xml.etree.ElementTree as ET
 from pe2loaddata.__main__ import headless as pe2loaddata
+import yaml
 
 s3 = boto3.client("s3")
 
@@ -16,39 +17,40 @@ def lambda_handler(event, lambda_context):
     batch = event["batch"]
     bucket = event["bucket"]
 
-    # (Eventually) Update config.yml based on Index.ids.xml file
-    s3xmlfilepath = (
-        f"projects/{project_name}/{batch}/images/{fullplate}/Images/Index.idx.xml"
-    )
-    localxmlfilepath = "/tmp/Index.idx.xml"
+    """
+    channeldict = event["channeldict"]
 
-    with open(localxmlfilepath, "wb") as f:
-        try:
-            s3.download_fileobj(bucket, s3xmlfilepath, f)
-        except botocore.exceptions.ClientError as error:
-            print("Can't find the xml file. Something's up.")
+
+    if channeldict == 'Standard_1BF':
+    if channeldict == 'Standard_3BF':
+    else:
+        if type(channeldict) is not dict:
+            print("In metadata: enter string for existing map into channeldict enter new map as dictionary.")
             return
-    with open(localxmlfilepath, "r") as input_xml:
-        xmlfile = ET.parse(input_xml)
-        root = xmlfile.getroot()
+    else:
 
-    yamlpath = "./config.yml"
-    # (Eventually) Open yaml
-    # (Eventually) Edit yaml to match xml
+
+    # Load and edit config file
+    config_file = "./config.yml"
+    with open(config_file, "r") as fd:
+        config = yaml.load(fd, Loader=yaml.BaseLoader)
+    config['channels'] =
+    # Write to yaml
+    """
+
+    config_file = "./config.yml"
 
     # Trigger pe2loaddata
-    output = f"/tmp/{project_name}/workspace/load_data_csv/{batch}/{plate}/load_data.csv"
+    output = f"/tmp/load_data.csv"
     index_directory = f"s3://{bucket}/projects/{project_name}/{batch}/images/"
-    index_file = f"s3://{bucket}/projects/{project_name}/{batch}/images/{plate}/Images/Index.idx.xml"
-    illum_directory = (
-        f"s3://{bucket}/projects/{project_name}/{batch}/illum/{plate}"
-    )
-    illum_output = f"/tmp/{project_name}/workspace/load_data_csv/{batch}/{plate}/load_data_with_illum.csv"
-    sub_string_out = f"s3://"
-    sub_string_in = f"bucket/"
+    index_file = f"s3://{bucket}/projects/{project_name}/{batch}/images/{fullplate}/Images/Index.idx.xml"
+    illum_directory = f"projects/{project_name}/{batch}/illum/{plate}"
+    illum_output = f"/tmp/load_data_with_illum.csv"
+    sub_string_out = "projects"
+    sub_string_in = "/home/ubuntu/bucket/projects"
 
     pe2loaddata(
-    yamlpath,
+    config_file,
     output,
     index_directory=index_directory,
     index_file=index_file,
@@ -57,5 +59,14 @@ def lambda_handler(event, lambda_context):
     plate_id=plate,
     illum_output=illum_output,
     sub_string_out=sub_string_out,
-    sub_string_in=sub_string_in
+    sub_string_in=sub_string_in,
+    search_subdirectories=True
     )
+
+    # Upload .csvs to S3
+    output_on_bucket_name = f"projects/{project_name}/workspace/load_data_csv/{batch}/{plate}/load_data.csv"
+    illum_output_on_bucket_name = f"projects/{project_name}/workspace/load_data_csv/{batch}/{plate}/load_data_with_illum.csv"
+    with open(output, "rb") as a:
+        s3.put_object(Body=a, Bucket=bucket, Key=output_on_bucket_name)
+    with open(illum_output, "rb") as a:
+        s3.put_object(Body=a, Bucket=bucket, Key=illum_output_on_bucket_name)
