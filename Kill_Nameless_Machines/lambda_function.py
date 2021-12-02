2ae4bef9fc43b40ca199569633f6656fd32c189a
ErinWeisbart <eweisbar@broadinstitute.org>
2021-12-02 14:08:50 -0800

    debug spot fleet killing

--- Kill_Nameless_Machines/lambda_function.py
+++ Kill_Nameless_Machines/lambda_function.py
@@ -5,50 +5,52 @@ from collections import Counter
 ec2 = boto3.client("ec2")
 sqs = boto3.client("sqs")
 
-queue_url = 'https://sqs.us-east-1.amazonaws.com/500910614606/Killed_Machines_List'
+# Set the queue for each
+queue_url = "https://sqs.us-east-1.amazonaws.com/500910614606/Killed_Machines_List"
 
 
 def check_if_named_or_spot_or_spot(instance_id):
     instance = ec2.describe_instances(InstanceIds=[instance_id])
-    if 'Tags' in instance['Reservations'][0]['Instances'][0]:
-        for tag in instance['Reservations'][0]['Instances'][0]['Tags']:
-            if 'Name' in tag['Key']:
-                instance_name = tag['Value']
-                print (f"{instance_id} has a name - {instance_name}.")
+    if "Tags" in instance["Reservations"][0]["Instances"][0]:
+        for tag in instance["Reservations"][0]["Instances"][0]["Tags"]:
+            if "Name" in tag["Key"]:
+                instance_name = tag["Value"]
+                print(f"{instance_id} has a name - {instance_name}.")
                 named = True
             else:
-                print (f"{instance_id} does not have a name.")
+                print(f"{instance_id} does not have a name.")
                 named = False
 
-            if 'ec2spot' in tag['Key']:
-                spot_request_id = tag['Value']
-                print (f"{instance_id} is in spot fleet {spot_request_id}.")
+            if "ec2spot" in tag["Key"]:
+                spot_request_id = tag["Value"]
+                print(f"{instance_id} is in spot fleet {spot_request_id}.")
             else:
                 spot_request_id = False
-                print (f"{instance_id} is not in a spot fleet.")
+                print(f"{instance_id} is not in a spot fleet.")
     else:
-        print (f"{instance_id} does not have a name and is not in a spot fleet.")
+        print(f"{instance_id} does not have a name and is not in a spot fleet.")
         named = False
         spot_request_id = False
     return named, spot_request_id
 
+
 def lambda_handler(event, lambda_context):
-    instance_id = event['detail']['instance-id']
-    time.sleep(60) # Wait 1 minute
+    instance_id = event["detail"]["instance-id"]
+    time.sleep(60)  # Wait 1 minute
 
     named, spot_request_id = check_if_named_or_spot(instance_id)
     if named:
         return
     else:
-        print ("Checking again in 2 minutes.")
-        time.sleep(60*2) # Wait 2 minutes
+        print("Checking again in 2 minutes.")
+        time.sleep(60 * 2)  # Wait 2 minutes
 
     named, spot_request_id = check_if_named_or_spot(instance_id)
     if named:
         return
     else:
-        print ("Checking again in 10 minutes.")
-        time.sleep(60*10) # Wait 10 minutes
+        print("Checking again in 10 minutes.")
+        time.sleep(60 * 10)  # Wait 10 minutes
 
     named, spot_request_id = check_if_named_or_spot(instance_id)
     if named:
@@ -56,36 +58,63 @@ def lambda_handler(event, lambda_context):
     else:
         if spot_request_id:
             # Add nameless instance to sqs queue
-            message = {'Instance':instance_id, 'Spot_Fleet':spot_request_id}
+            message = f"{instance_id} {spot_request_id}"
             sqs.send_message(QueueUrl=queue_url, MessageBody=message)
 
-            queue_messages = sqs.receive_message(QueueUrl=queue_url, MaxNumberOfMessages = 10)
+            # Must iterate to get all messages in queue
+            queue_messages = []
+            while True:
+                message_set = sqs.receive_message(
+                    QueueUrl=queue_url,
+                    AttributeNames=["SentTimestamp"],
+                    MaxNumberOfMessages=10,
+                    WaitTimeSeconds=20,
+                )
+                try:
+                    queue_messages.extend(message_set["Messages"])
+                except:
+                    break
+
             fleetlist = []
-            for msg in queue_messages['Messages']:
+            for msg in queue_messages:
                 # List nameless spot fleet machines killed in last hour
-                if time.time() - msg['Attributes']['SentTimestamp'] < 60*60:
-                    fleet = msg['Body']['Spot_Fleet']
+                if (
+                    time.time() - int(msg["Attributes"]["SentTimestamp"]) / 1000
+                    < 60 * 60
+                ):
+                    fleet = msg["Body"].split(" ")[1]
                     fleetlist.append(fleet)
                 # Clean up while we're at it: Delete messages that are over a day old
-                if time.time() - msg['Attributes']['SentTimestamp'] > 60*60*24:
-                    sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=msg['ReceiptHandle'])
+                if (
+                    time.time() - int(msg["Attributes"]["SentTimestamp"]) / 1000
+                    > 60 * 60 * 24
+                ):
+                    sqs.delete_message(
+                        QueueUrl=queue_url, ReceiptHandle=msg["ReceiptHandle"]
+                    )
             # If more than 2 nameless machines killed in last hour in spot fleet
             countdict = Counter(fleetlist)
             if countdict[spot_request_id] > 2:
-                activeinstances = ec2.describe_spot_fleet_instances(SpotFleetRequestId=spot_request_id)
-                for active in activeinstances['ActiveInstances']:
-                    instance_id = instance['InstanceId']
+                activeinstances = ec2.describe_spot_fleet_instances(
+                    SpotFleetRequestId=spot_request_id
+                )
+                for active in activeinstances["ActiveInstances"]:
+                    instance_id = instance["InstanceId"]
                     instance = ec2.describe_instances(InstanceIds=[instance_id])
                     # Check if any machines in spot fleet are named
-                    for tag in instance['Reservations'][0]['Instances'][0]['Tags']:
-                        if 'Name' in tag['Key']:
+                    for tag in instance["Reservations"][0]["Instances"][0]["Tags"]:
+                        if "Name" in tag["Key"]:
                             has_named_instance = True
                         else:
                             has_named_instance = False
             # If no machines in spot fleet are named, kill it
             if not has_named_instance:
-                print (f"{spot_request_id} has no named instances. Cancelling spot request.")
-                ec2.cancel_spot_fleet_requests(SpotFleetRequestIds=[spot_request_id], TerminateInstances=True)
+                print(
+                    f"{spot_request_id} has no named instances. Cancelling spot request."
+                )
+                ec2.cancel_spot_fleet_requests(
+                    SpotFleetRequestIds=[spot_request_id], TerminateInstances=True
+                )
 
-        print (f"{instance_id} is nameless for too long after launch. Terminating.")
+        print(f"{instance_id} is nameless for too long after launch. Terminating.")
         ec2.terminate_instances(InstanceIds=[instance_id])
